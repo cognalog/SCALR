f0c0e055d9e0ae15d4eb270c385e57f3d630edd7
Dobri Dobrev <dobridobrev@dyn-160-39-206-25.dyn.columbia.edu>
2013-04-17 21:50:11 -0400

    dobri's first commit TADAAAAAA3

--- scalr/variable/Number.java
+++ scalr/variable/Number.java
@@ -3,12 +3,26 @@ package scalr.variable;
 
 public class Number implements Variable
 {
+	private int val;
+	public Number(int i){
+		val=i;
+	}
+	public void setValue(int i){
+		val=i;
+	}
+	public void modValue(int i){
+		val+=i;
+	}
+	
 	
 	@Override
-	public Variable getValue()
+	public Variable getCopy()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return this.clone();
+	}
+	public Number clone(){
+		return new Number(val);
 	}
 	
 }
--- scalr/variable/Sequence.java
+++ scalr/variable/Sequence.java
@@ -1,14 +1,38 @@
 
 package scalr.variable;
+import java.util.ArrayList;
 
 public class Sequence implements Variable
 {
+	ArrayList<Note> theNotes;
+	public Sequence(){
+		theNotes= new ArrayList<Note>();
+	}
+	private void changeSeq(ArrayList<Note> l){
+		theNotes=(ArrayList<Note>) l.clone();
+	}
 	
+	
+	public void addNoteToEnd(Note e){
+		theNotes.add(e);
+	}
+	public void deleteLeftmost(){
+		theNotes.remove(0);
+	}
+	public void deleteRightmost(){
+		int t= theNotes.size()-1;
+		theNotes.remove(t);
+	}
 	@Override
-	public Variable getValue()
+	public Variable getCopy()
 	{
 		// TODO Auto-generated method stub
-		return null;
+		return (Sequence) this.clone();
+	}
+	public Object clone(){
+		Sequence temp= new Sequence();
+		temp.changeSeq(theNotes);
+		return temp;
 	}
 	
 }
