40e122dabc2d89f842e7b8f06fd940073f15bafb
kugurst <ma2799@gmail.com>
2013-05-01 17:18:18 -0400

    Small changes.
    Removed the "make" from the refresh target. Passed the root node into BackendMain.java. Now, shit just got serious. It might be a while before my next commit.

--- scalr/BackendMain.java
+++ scalr/BackendMain.java
@@ -1,11 +1,13 @@
 
 package scalr;
 
+import parser.SimpleNode;
+
 public class BackendMain
 {
 	public static int	tempo	= 120;
 	
-	public BackendMain()
+	public BackendMain(SimpleNode node)
 	{
 		System.out.println(tempo);
 	}
