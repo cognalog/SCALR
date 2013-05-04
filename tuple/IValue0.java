574d025f81a868b1e8fb013f0b53c90bbeca509c
Kofi Opoku <kso2110@columbia.edu>
2013-05-03 22:49:41 -0400

    adding tuple package

---
+++ tuple/IValue0.java
@@ -0,0 +1,19 @@
+package scalr.tuple;
+
+/**
+ * 
+ * @adapted Kofi Opoku
+ * 
+ *          This tuple package has been adapted from the JAVATUPLES package
+ *          written by Daniel Fernandez.
+ */
+public interface IValue0<X> {
+
+	/**
+	 * Interface used for the pair and triplet
+	 * 
+	 * @return
+	 */
+	public X getValue0();
+
+}
