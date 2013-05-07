5932a89831b356420ec8f8180ac03204b3be5bfa
kugurst <ma2799@gmail.com>
2013-05-07 17:05:43 -0400

    Committing my working math.
    unary\(\) still needs some work, but otherwise, it produces correct ASTs. There's another problem, which is that IDs don't necessarily link to an instance of a variable. In otherwords, the semantic actions for something... I can't seem to find it at the moment, are incorrect.

--- scalr/expression/UnaryOperator.java
+++ scalr/expression/UnaryOperator.java
@@ -1,11 +1,14 @@
 
 package scalr.expression;
 
+import scalr.variable.ScalrNum;
+
 public class UnaryOperator implements Expression
 {
 	String	   operator;
 	Expression	expr;
 	
+	// This class is only valid for integers
 	public UnaryOperator(String op)
 	{
 		operator = op;
@@ -19,15 +22,33 @@ public class UnaryOperator implements Expression
 	@Override
 	public Expression getValue(Expression... expressions)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		if (expr.getType() != ExpressionType.NUMBER)
+			return null;
+		else {
+			if (operator.equals("-")) {
+				ScalrNum num = (ScalrNum) expr.getValue(expressions);
+				return new ScalrNum(-num.getNum());
+			}
+			// These two functions are a little more interesting, as they only make sense on a
+			// variable in the symbol table. We have to modify those variables, and not make a copy
+			// of them
+			else if (operator.equals("--")) {
+				ScalrNum num = (ScalrNum) expr.getValue(expressions);
+				num.setValue(num.getNum() - 1);
+				return new ScalrNum(num.getNum() + 1);
+			}
+			else {
+				ScalrNum num = (ScalrNum) expr.getValue(expressions);
+				num.setValue(num.getNum() + 1);
+				return new ScalrNum(num.getNum() - 1);
+			}
+		}
 	}
 	
 	@Override
 	public ExpressionType getType()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return ExpressionType.NUMBER;
 	}
 	
 }
