7480c7eeef0498517d80b4271848cd52f3c79e84
kugurst <ma2799@gmail.com>
2013-05-11 16:22:57 -0400

    The Sequence class should be good to go.
    It has some useless functions, and not enough null-checking, but the getValue() method actually evaluates all the expressions it has. It also automatically flattens sequences (by evaluating them), so there should be no more need for flattenToString.

--- scalr/variable/Sequence.java
+++ scalr/variable/Sequence.java
@@ -8,78 +8,100 @@ import scalr.expression.ExpressionType;
 
 public class Sequence implements Variable
 {
-	ArrayList<Expression>	theNotes;
+	ArrayList<Expression>	notes;
 	
 	public Sequence()
 	{
-		theNotes = new ArrayList<Expression>();
+		notes = new ArrayList<Expression>();
 	}
 	
 	public Sequence(Expression... notes)
 	{
-		theNotes = new ArrayList<Expression>(notes.length + 10);
+		this.notes = new ArrayList<Expression>(notes.length + 10);
 		for (Expression n : notes)
-			theNotes.add(n);
+			this.notes.add(n);
 	}
 	
 	public Note getNote(String index)
 	{
-		return (Note) theNotes.get(Integer.parseInt(index)).getValue();
+		return (Note) notes.get(Integer.parseInt(index)).getValue();
 	}
 	
 	public void extend(int ext)
 	{
-		ArrayList<Expression> extendedNotes = new ArrayList<Expression>(theNotes.size() * ext + 10);
+		ArrayList<Expression> extendedNotes = new ArrayList<Expression>(notes.size() * ext + 10);
 		for (int i = 0; i < ext; i++)
-			for (Expression n : theNotes)
+			for (Expression n : notes)
 				extendedNotes.add(n);
-		theNotes = extendedNotes;
+		notes = extendedNotes;
 	}
 	
 	public void addSequence(Sequence seq)
 	{
-		for (Expression n : seq.theNotes)
-			theNotes.add(n);
+		for (Expression n : seq.notes)
+			notes.add(n);
 	}
 	
 	public void addNoteToEnd(Expression e)
 	{
-		theNotes.add(e);
+		notes.add(e);
 	}
 	
 	public void addNoteToStart(Expression e)
 	{
-		if (theNotes.size() > 0)
-			theNotes.add(0, e);
+		if (notes.size() > 0)
+			notes.add(0, e);
 		else
-			theNotes.add(e);
+			notes.add(e);
 	}
 	
 	public void deleteLeftmost()
 	{
-		theNotes.remove(0);
+		notes.remove(0);
 	}
 	
 	public void deleteRightmost()
 	{
-		int t = theNotes.size() - 1;
-		theNotes.remove(t);
+		int t = notes.size() - 1;
+		notes.remove(t);
 	}
 	
 	@Override
 	public Variable getCopy()
 	{
-		ArrayList<Expression> copy = new ArrayList<Expression>(theNotes.size());
-		for (Expression n : theNotes)
+		ArrayList<Expression> copy = new ArrayList<Expression>(notes.size());
+		for (Expression n : notes)
 			copy.add(n);
 		Sequence seq = new Sequence();
-		seq.theNotes = copy;
+		seq.notes = copy;
 		return seq;
 	}
 	
+	/**
+	 * The getValue() method of a sequence evaluates all the expressions within the sequence.
+	 */
 	@Override
 	public Expression getValue(Expression... expressions)
 	{
+		for (int i = 0; i < notes.size(); i++) {
+			Expression expr = notes.get(i);
+			Expression result = expr.getValue(expressions);
+			if (result.getType() == ExpressionType.NOTE)
+				notes.set(i, result);
+			else if (result.getType() == ExpressionType.SEQUENCE) {
+				notes.remove(i);
+				ArrayList<Expression> seq = ((Sequence) result).notes;
+				for (int j = 0; j < seq.size(); j++)
+					notes.add(i + j, seq.get(j));
+			}
+			else {
+				System.err.println(result + " is not of type Sequence or Note.");
+				StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+				for (StackTraceElement elem : stack)
+					System.err.println(elem);
+				System.exit(1);
+			}
+		}
 		return this;
 	}
 	
@@ -97,12 +119,12 @@ public class Sequence implements Variable
 		String result = "[";
 		// Append each note to the string
 		// for(int i = 0; i < theNotes.size(); i++)
-		for (Expression n : theNotes) {
+		for (Expression n : notes) {
 			result += n.getValue().toString() + "|";
 			
 		}
 		// remove the last bar (if there is a note)
-		if (theNotes.size() > 0)
+		if (notes.size() > 0)
 			result = result.substring(0, result.length() - 1);
 		// And it closes with a bracket
 		return result + "]";
@@ -114,7 +136,7 @@ public class Sequence implements Variable
 		String result = "[";
 		// Append each note to the string
 		// for(int i = 0; i < theNotes.size(); i++)
-		for (Expression n : theNotes) {
+		for (Expression n : notes) {
 			String temp = n.getValue().toString();
 			if (n.getType() == ExpressionType.SEQUENCE) {
 				temp = temp.substring(1, temp.length() - 1);
@@ -123,7 +145,7 @@ public class Sequence implements Variable
 			
 		}
 		// remove the last bar (if there is a note)
-		if (theNotes.size() > 0)
+		if (notes.size() > 0)
 			result = result.substring(0, result.length() - 1);
 		// And it closes with a bracket
 		return result + "]";
