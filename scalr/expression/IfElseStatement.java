8c990b97ea7e04b0ffd0f19cd98e532b41e009b8
kugurst <ma2799@gmail.com>
2013-05-11 00:20:54 -0400

    Changed the grammar to print the stack trace on failure.

--- scalr/expression/IfElseStatement.java
+++ scalr/expression/IfElseStatement.java
@@ -1,11 +1,38 @@
+
 package scalr.expression;
+
+import java.util.ArrayList;
 
 public class IfElseStatement implements Expression
 {
+	ArrayList<Expression>	         checks	       = new ArrayList<Expression>();
+	ArrayList<ArrayList<Expression>>	statements	= new ArrayList<ArrayList<Expression>>();
+	private int	                     addPosition	= -1;
 	
 	public IfElseStatement()
+	{}
+	
+	public void addIf(Expression expr)
 	{
-		// TODO Auto-generated constructor stub
+		if (++addPosition == 0) {
+			checks.add(expr);
+			statements.add(new ArrayList<Expression>());
+		}
+	}
+	
+	public void addElIf(Expression expr)
+	{	
+		
+	}
+	
+	public void addEl(Expression expr)
+	{	
+		
+	}
+	
+	public void addStatement(Expression expr)
+	{	
+		
 	}
 	
 	@Override
