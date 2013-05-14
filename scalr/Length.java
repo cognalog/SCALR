2e9d1c9baf317a95b58d7ad792b90107fa8b52a0
kugurst <ma2799@gmail.com>
2013-05-14 14:13:30 -0400

    Also added wholeT and helfT to lengths, as well as to the allowed lengths.

--- scalr/Length.java
+++ scalr/Length.java
@@ -3,7 +3,8 @@ package scalr;
 
 public enum Length {
 	thirtysecT("1/48"), thirtysec("1/32"), sixteenthT("1/24"), sixteenth("1/16"), eighthT("1/12"),
-	eighth("1/8"), quarterT("1/6"), quarter("1/4"), half("1/2"), whole("1");
+	eighth("1/8"), quarterT("1/6"), quarter("1/4"), halfT("1/3"), half("1/2"), wholeT("2/3"),
+	whole("1");
 	
 	public final String	duration;
 	
