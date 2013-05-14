172a9d3e7ff972e0c3d80dc02f1d4184f61b1d99
kugurst <ma2799@gmail.com>
2013-05-14 10:47:58 -0400

    Added a pitch() and duration() operator with no extra conflicts, so nothing should have broken.
    The pitch operator takes a note as its argument, and returns the ordinal of the pitch, not the midivalue.
    The pitch operator takes a note or sequence as its argument, and returns the ordinal of the length, if the argument is a note, or the number of notes in a sequence.

---
+++ scalr/expression/GetOperator.java
@@ -0,0 +1,80 @@
+
+package scalr.expression;
+
+import scalr.variable.Note;
+import scalr.variable.ScalrNum;
+import scalr.variable.Sequence;
+
+public class GetOperator implements Expression
+{
+	Expression	   operand;
+	private String	operator;
+	
+	/**
+	 * This class returns the length of a note, or the number of notes in a sequence.
+	 */
+	public GetOperator(String operator)
+	{
+		this.operator = operator;
+	}
+	
+	public void addOperand(Expression expr)
+	{
+		operand = expr;
+	}
+	
+	@Override
+	public Expression getValue(Expression... expressions)
+	{
+		// Type Checking
+		if (operator.equals("dur")
+		        && (operand.getType() != ExpressionType.SEQUENCE && operand.getType() != ExpressionType.NOTE)) {
+			System.err.println("The duration operator is only valid for notes and sequences.");
+			StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+			for (StackTraceElement elem : stack)
+				System.err.println("\t" + elem);
+			System.exit(1);
+		}
+		else if (operator.equals("pit") && operand.getType() != ExpressionType.NOTE) {
+			System.err.println("The pitch operator is only valid for notes.");
+			StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+			for (StackTraceElement elem : stack)
+				System.err.println("\t" + elem);
+			System.exit(1);
+		}
+		if (operator.equals("dur")) {
+			if (operand.getType() == ExpressionType.SEQUENCE) {
+				Sequence seq = (Sequence) operand.getValue(expressions);
+				return new ScalrNum(seq.getSequence().size());
+			}
+			else if (operand.getType() == ExpressionType.NOTE) {
+				Note note = (Note) operand.getValue(expressions);
+				return new ScalrNum(note.length.ordinal());
+			}
+		}
+		else if (operator.equals("pit")) {
+			if (operand.getType() == ExpressionType.NOTE) {
+				Note note = (Note) operand.getValue(expressions);
+				return new ScalrNum(note.pitch.ordinal());
+			}
+		}
+		// Someone used this function inappropriately.
+		return null;
+	}
+	
+	@Override
+	public ExpressionType getType()
+	{
+		if (operator.equals("dur")) {
+			if (operand.getType() == ExpressionType.NOTE
+			        || operand.getType() == ExpressionType.SEQUENCE)
+				return ExpressionType.NUMBER;
+		}
+		else if (operator.equals("pit")) {
+			if (operand.getType() == ExpressionType.NOTE)
+				return ExpressionType.NUMBER;
+		}
+		return null;
+	}
+	
+}
