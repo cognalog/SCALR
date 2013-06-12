a78ce0c79268f11326d80eab76d9502785224b66
kugurst <ma2799@gmail.com>
2013-06-12 13:27:10 -0400

    Switching to Linux. Committing my changes thus far.
    I'm tired of commenting.

--- scalr/expression/ControlOperation.java
+++ scalr/expression/ControlOperation.java
@@ -3,35 +3,42 @@ package scalr.expression;
 
 public class ControlOperation implements Expression
 {
-	
+
 	private String	   operation;
 	private Expression	ret;
-	
+
 	public ControlOperation(String op)
 	{
 		operation = op;
 	}
-	
+
 	public void addReturn(Expression expr)
 	{
 		ret = expr;
 	}
-	
+
+	/**
+	 * Returns the {@linkplain Expression} that is part of this <code>{@link scalr.expression.ExpressionType}
+	 * .RETURN</code> statement. It guarantees that the return is a {@linkplain scalr.variable.Sequence}.
+	 *
+	 * @return Always returns an <code>instanceof</code> {@linkplain scalr.variable.Sequence}.
+	 */
 	public Expression getReturn()
 	{
+
 		return ret;
 	}
-	
+
 	@Override
 	public Expression getValue()
 	{
 		return this;
 	}
-	
+
 	@Override
 	public ExpressionType getType()
 	{
 		return ExpressionType.valueOf(operation.toUpperCase());
 	}
-	
+
 }
--- scalr/expression/Function.java
+++ scalr/expression/Function.java
@@ -11,6 +11,14 @@ import scalr.variable.Sequence;
 import scalr.variable.SymbolTable;
 import scalr.variable.Variable;
 
+/**
+ * The workhorse of SCALR. While not complex in code, {@linkplain Function}s were designed to be agnostic of each
+ * other, as all {@linkplain Expression}s. It was a bit trickier with the execution of a {@linkplain Function},
+ * but the basic premise is to compile a list of {@linkplain Expression}-statements,
+ * and then evaluate them in the order they were received. However, a {@linkplain Function} is more or less a
+ * skeleton for the execution of code, it should not change with it, which is the reason why all {@linkplain
+ * Variable}s now return a copy of themselves in the <code>getValue()</code> method.
+ */
 public class Function implements Expression
 {
 	/** The name of the function */
@@ -18,16 +26,25 @@ public class Function implements Expression
 	private ArrayList<String>	       parameterName;
 	public ArrayList<Expression>	   statements;
 	private Expression[]	           parameterValues;
-	private HashMap<String, Expression>	symbolTable;
 
+	/**
+	 * Constructs a {@linkplain Function} with the name of the function, as that is the first bit of information
+	 * known. It also initializes all relevant fields.
+	 * @param name The {@linkplain String} corresponding to the name of this {@linkplain Function}.
+	 */
 	public Function(String name)
 	{
 		id = name;
 		parameterName = new ArrayList<String>();
 		statements = new ArrayList<Expression>();
-		symbolTable = new HashMap<String, Expression>();
 	}
 
+	/**
+	 * Adds a parameter to this {@linkplain Function}. This method is not used to add the value of the parameter,
+	 * but just the {@linkplain String} corresponding to the ID of the parameter. When evaluated,
+	 * the arguments will be evaluated and added to the symbol table in the order that they were added.
+	 * @param name The {@linkplain String} corresponding to the name of this parameter. Must be unique.
+	 */
 	public void addParameter(String name)
 	{
 		if (parameterName.contains(name))
@@ -35,22 +52,53 @@ public class Function implements Expression
 		parameterName.add(name);
 	}
 
+	/**
+	 * Adds the <code>{@linkplain Expression}s</code> corresponding to the arguments of this function. This method is
+	 * made variadic to allow no arguments. The number of arguments should be the same as the number of parameters.
+	 * @param expressions The <code>{@linkplain Expression}[]</code> to set this {@linkplain Function}s parameters to
+	 *                       . Only considers the last array added.
+	 */
 	public void addValues(Expression... expressions)
 	{
 		this.parameterValues = expressions;
 	}
 
+	/**
+	 * Adds an {@linkplain Expression}-statement to the list of statements of this {@linkplain Function}. It can be
+	 * any {@linkplain Expression}, so long as it is not null.
+	 * @param expr An {@linkplain Expression} to add to the list of statements of this {@linkplain Function}.
+	 */
 	public void addStatement(Expression expr)
 	{
 		if (expr != null)
 			statements.add(expr);
 	}
 
+	/**
+	 * Returns the name of this {@linkplain Function}. Used mostly internally.
+	 *
+	 * @return A {@linkplain String} corresponding to the name of this {@linkplain Function}.
+	 */
 	public String getName()
 	{
 		return id;
 	}
 
+	/**
+	 * Evaluates this {@linkplain Function}. The process is this:
+	 * <ul>
+	 *     <li>Check the number of arguments</li>
+	 *     <li>Evaluate them and store them in the symbol table</li>
+	 *     <li>Evaluate the expression and check for any return statements</li>
+	 *     <li>Restore the previous symbol table</li>
+	 *     <li>Return</li>
+	 * </ul>
+	 * A series of checks is performed to ensure that an illegal state does not occur,
+	 * and the return is checked to make sure it is a "sequence" ({@linkplain scalr.variable.Note}s are automatically
+	 * promoted to a {@linkplain Sequence}).
+	 *
+	 * @return Always returns an <code>instanceof</code> {@linkplain Sequence}.
+	 */
 	@Override
 	public Expression getValue()
 	{
@@ -61,10 +109,11 @@ public class Function implements Expression
 			System.exit(1);
 		}
 		// Add the parameters to our symbol table
+		// Create the symbol table
+		HashMap<String, Expression> symbolTable = new HashMap<String, Expression>();
 		// Evaluate the parameters in the scope of the previous function
 		for (int i = 0; i < parameterValues.length; i++)
-			symbolTable.put(parameterName.get(i),
-			        ((Variable) parameterValues[i].getValue()).getCopy());
+			symbolTable.put(parameterName.get(i), ((Variable) parameterValues[i].getValue()).getCopy());
 
 		// Mark the old symbol table
 		HashMap<String, Expression> parentTable = SymbolTable.currentSymbolTable;
@@ -109,6 +158,15 @@ public class Function implements Expression
 		return lastExpr;
 	}
 
+	/**
+	 * Returns a copy of this {@linkplain Function} object. By copy, it returns a shallow copy of the statements and
+	 * parameter names. That is to say, the underlying {@linkplain Expression}s in the list of statements are the
+	 * same for each {@linkplain Function} object. This lead to the reason that {@linkplain Variable}s all had to
+	 * return a copy of themselves, because otherwise they would be permanently changed when they were evaluated.
+	 *
+	 * @return A {@linkplain Function} that has the same list of statements and parameters as this function. The
+	 * parameter values are not copied.
+	 */
 	public Function getCopy()
 	{
 		Function func = new Function(id);
@@ -117,12 +175,22 @@ public class Function implements Expression
 		return func;
 	}
 
+	/**
+	 * As defined by the LRM, {@linkplain Function}s always returns {@linkplain Sequence}s.
+	 *
+	 * @return Always returns <code>{@link scalr.expression.ExpressionType}.SEQUENCE</code>.
+	 */
 	@Override
 	public ExpressionType getType()
 	{
 		return ExpressionType.SEQUENCE;
 	}
 
+	/**
+	 * A utility method to print the JVM stack trace. Used for hand-written errors.
+	 *
+	 * @param message A {@linkplain String} to use as the first line in the stack trace.
+	 */
 	public static void printStackTrace(String message)
 	{
 		System.err.println(message);
@@ -131,6 +199,11 @@ public class Function implements Expression
 			System.err.println("\t" + stack[i]);
 	}
 
+	/**
+	 * Mostly for debugging. Returns a string of the form "(Func: &lt;NAME&gt; [{PARAMETERS}])".
+	 *
+	 * @return A {@linkplain String} as defined above.
+	 */
 	@Override
 	public String toString()
 	{
